0cbaf378fe8d53ace631ed6b04968d9595b7770b
Luis Mendoza <luismendoza@Luiss-MacBook-Pro.local>
2017-12-07 12:21:59 -0800

    updated port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -22,3 +22,4 @@ var routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
 
 app.listen(port);
+
